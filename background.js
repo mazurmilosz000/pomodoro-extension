ada400e2bcd3e4ae3fc9926048b68b6e5117f7db
Milosz Mazur <mazur.milosz000@gmail.com>
2022-07-18 17:28:40 +0200

    added working timer

--- background.js
+++ background.js
@@ -1,11 +1,30 @@
 chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name === "Pomodoro"){
+    if (alarm.name === 'Pomodoro'){
         console.log("alarm!!!!!")
         // doesn't work properly TODO: fix code below!!!
-        chrome.storage.local.get(["running"], (res)=>{
-            console.log(res.running)
+        chrome.storage.local.get(['running', 'time'], (res)=>{
+            if(res.running){
+                let timer = res.time-1
+                chrome.storage.local.set({'time': timer})
+                if (timer == 0){
+                    this.registration.showNotification('Pomodoro Timer', {
+                        body: `Time's up! Click here to start 5 minute break`,
+                        icon: 'icon.png',
+                        
+                    })
+                    chrome.storage.local.set({'running': !res.running})
+
+                }
+            }
         })
     }
+})
+
+self.addEventListener('notificationclick', ()=>{
+    chrome.storage.local.set({
+        'time': 300,
+        'running': true
+    })
 })
 
 chrome.runtime.onInstalled.addListener(()=>{
--- popup.js
+++ popup.js
@@ -1,14 +1,50 @@
+function update_countdown(){
+    chrome.storage.local.get(['time', 'running'], (res) => {
+        const countdown_element = document.getElementById('current_time')
+        let time = res.time
+        console.log(time)
+        console.log('is running?: ', res.running)
+        let minutes = Math.floor(time / 60)
+        let seconds = time % 60
+
+        // if seconds are less than 10 add 0 before
+        seconds = seconds < 10 ? '0' + seconds : seconds
+        minutes = minutes < 10 ? '0' + minutes : minutes
+
+
+        countdown_element.innerHTML = `${minutes}:${seconds}`
+        // document.getElementById('time_selector').innerHTML = res.running ? 'Study Time!' : 'Paused!'
+        document.getElementById('start_button').innerHTML = res.running ? 'STOP' : 'START'
+
+    })
+
+}
+
+
+const resetButton = document.getElementById('reset_button')
+resetButton.addEventListener('click', () => {
+    chrome.storage.local.set({
+        'running': false,
+        'time': 0
+    })
+})
+
+update_countdown()
+
+// call the function every second
+setInterval(update_countdown, 1000)
+
 document.getElementById("t_15").addEventListener("click", () => {
 
     document.getElementById('current_time').innerHTML = '15:00'
-    localStorage.setItem('time', 900)
+    chrome.storage.local.set({'time': 900})
 
 });
 
 document.getElementById("t_25").addEventListener("click", () => {
 
     document.getElementById('current_time').innerHTML = '25:00'
-    localStorage.setItem('time', 1500)
+    chrome.storage.local.set({'time': 1500})
 
 
 });
@@ -16,14 +52,14 @@ document.getElementById("t_25").addEventListener("click", () => {
 document.getElementById("t_35").addEventListener("click", () => {
 
     document.getElementById('current_time').innerHTML = '35:00'
-    localStorage.setItem('time', 2100)
+    chrome.storage.local.set({'time': 2100})
 
 });
 
 document.getElementById("t_45").addEventListener("click", () => {
 
     document.getElementById('current_time').innerHTML = '45:00'
-    localStorage.setItem('time', 2700)
+    chrome.storage.local.set({'time': 2700})
 
 });
 
@@ -32,35 +68,12 @@ document.getElementById("t_45").addEventListener("click", () => {
 document.getElementById("start_button").addEventListener("click", ()=>
 {
 
-    const countdown_element = document.getElementById('current_time')
-
-    // todo: fix code below (how to chrome storage local set????)
-    chrome.storage.local.set({"running": true}, ()=>{
-        console.log('Timer is running!')
-    })
-
-    chrome.alarms.create("Pomodoro", {
+    chrome.alarms.create('Pomodoro', {
         periodInMinutes: 1 / 60,
     })
 
-
-    // call the function every second
-    setInterval(update_countdown, 1000)
-
-    function update_countdown(){
-        let time = localStorage.getItem("time")
-        const minutes = Math.floor(time / 60)
-        let seconds = time % 60
-
-        // if seconds are less than 10 add 0 before
-        seconds = seconds < 10 ? '0' + seconds : seconds
-
-        countdown_element.innerHTML = `${minutes}:${seconds}`
-        time--
-        localStorage.setItem("time", time)
-    }
-    document.getElementById('time_selector').innerHTML = 'Study Time!'
-    document.getElementById('btn').innerHTML = '<button id="stop_button">STOP</button>'
-
-    
+    // change running value
+    chrome.storage.local.get(["running"], (res)=> {
+        chrome.storage.local.set({'running': !res.running})
+    })
 })
