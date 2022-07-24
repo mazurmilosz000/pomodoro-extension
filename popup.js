6c9ed801a36fa86315e165f3c9b64bd102c6bf9c
Milosz Mazur <mazur.milosz000@gmail.com>
2022-07-24 22:19:17 +0200

    added dark mode

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 function update_countdown(){
-    chrome.storage.local.get(['time', 'running'], (res) => {
+    chrome.storage.local.get(['time', 'running', 'dark_mode'], (res) => {
         const countdown_element = document.getElementById('current_time')
         let time = res.time
         console.log(time)
@@ -13,10 +13,14 @@ function update_countdown(){
 
 
         countdown_element.innerHTML = `${minutes}:${seconds}`
-        // document.getElementById('time_selector').innerHTML = res.running ? 'Study Time!' : 'Paused!'
         document.getElementById('start_button').innerHTML = res.running ? 'STOP' : 'START'
-
-    })
+        if (res.dark_mode) {
+            document.body.style.backgroundColor = "rgb(46, 46, 46)"
+        }
+        else {
+            document.body.style.backgroundColor = "rgb(227, 134, 139)"
+        }
+})
 
 }
 
@@ -92,13 +96,23 @@ document.getElementById("start_button").addEventListener("click", ()=>
     })
 
     // change running value
-    chrome.storage.local.get(["running"], (res)=> {
-        chrome.storage.local.set({'running': !res.running})
+    chrome.storage.local.get(['running', 'time'], (res)=> {
+            if (res.time != 0){
+                chrome.storage.local.set({'running': !res.running})
+            }
     })
 })
 
-
-// todo: change to dark mode
+// changing background color
 document.getElementById('change_mode').addEventListener('click', ()=>{
-    console.log('clicker')
+    chrome.storage.local.get(['dark_mode'], (res) => {
+        if (res.dark_mode) {
+            chrome.storage.local.set({'dark_mode': false})
+            document.body.style.backgroundColor = "rgb(227, 134, 139)"
+        }else {
+            chrome.storage.local.set({'dark_mode': true})
+            document.body.style.backgroundColor = "rgb(46, 46, 46)"
+        }
+    })
+
 })
